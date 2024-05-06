6a9d475ff90ee321a0d3bc5c562588066f65e272
gustavo.choque <gustvjor2005@gmail.com>
2024-05-06 17:26:52 -0500

    third commit

---
+++ blog-odin-ms/src/main/java/org/tech/controller/HomeController.java
@@ -0,0 +1,10 @@
+package org.tech.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HomeController {
+
+  private final static Logger log = LoggerFactory.getLogger(HomeController.class);
+
+}
